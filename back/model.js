734aaec416bd556beaafd14d3ddc984085077f06
saebyeokchu <cuu2252@gmail.com>
2023-03-14 15:59:06 +0900

    [ADD] create model

--- back/model.js
+++ back/model.js
@@ -1,12 +1,35 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-let details = new Schema({
-    name : {
+let content = new Schema({
+    _id : {
         type: String
     },
-    age : {
+    title : {
+        type : String
+    },
+    content : {
+        type : String
+    },
+    viewCount : {
         type : Number
+    },
+    commentCount : {
+        type : Number
+    },
+    createdAt : {
+        type : String
+    },
+    comments : {
+        commentId : {
+            type : String
+        },
+        content : {
+            type : String
+        },
+        createdAt : {
+            type : String
+        }
     }
 });
 
